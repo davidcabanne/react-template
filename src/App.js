5f0fd3b169d43cabe42808aa76dbd79dc1963cf3
David Cabanne <davidcbne@gmail.com>
2022-02-06 18:42:15 +0100

    [=>] React-template : css reset / node-sass / default component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Default from './Components/Default';
+
+function App() {
+  return (
+    <div className="App">
+     <Default />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Default.js
@@ -0,0 +1,13 @@
+import '../Styles/Default.css';
+
+function Default() {
+    return (
+        <div className="Default section__container">
+            <div className='section__wrapper'>
+                <h1>Davo's Boilerplate</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Default;
